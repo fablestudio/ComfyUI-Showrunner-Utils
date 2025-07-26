f416658910244d5822a4dd12ff6d0fbe957966cb
Mike Rowley <m@soze.biz>
2025-07-26 10:53:39 -0600

    Added Seed Node with String

---
+++ seed.py
@@ -0,0 +1,19 @@
+#Code from https://github.com/WASasquatch/was-node-suite-comfyui
+
+class SR_Seed:
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {"required":
+                {"seed": ("INT", {"default": 0, "min": 0,
+                          "max": 0xffffffffffffffff})}
+                }
+
+    RETURN_TYPES = ("SEED", "NUMBER", "FLOAT", "INT", "STRING")
+    RETURN_NAMES = ("seed", "number", "float", "int", "string")
+    FUNCTION = "seed"
+
+    CATEGORY = "utils"
+
+    def seed(self, seed):
+        return ({"seed": seed, }, seed, float(seed), int(seed), str(seed))
+
